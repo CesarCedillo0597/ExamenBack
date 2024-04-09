9571fb3b705121241d76b45668bc1b152344e3fa
Cesar Cedillo Clavijo <cesar.cedillo@agilethought.com>
2024-04-09 12:29:22 -0600

    configurando eureka

--- workers-microservice/src/main/java/com/ExamenBack/workersmicroservice/WorkersMicroserviceApplication.java
+++ workers-microservice/src/main/java/com/ExamenBack/workersmicroservice/WorkersMicroserviceApplication.java
@@ -2,11 +2,12 @@ package com.ExamenBack.workersmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.*;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.ExamenBack.workersmicroservice")
 public class WorkersMicroserviceApplication {
 
 	public static void main(String[] args) {
